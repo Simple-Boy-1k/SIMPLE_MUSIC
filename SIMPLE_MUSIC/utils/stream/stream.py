700ac137380fc4b4ddff2831c1793a21e6892e72
𝘚𝘪𝘮𝘱𝘭𝘦_𝘉𝘰𝘺 <sksahnawaj89@gmail.com>
2026-08-23 23:51:06 +0530

    Update stream.py

--- SIMPLE_MUSIC/utils/stream/stream.py
+++ SIMPLE_MUSIC/utils/stream/stream.py
@@ -2,14 +2,6 @@
 # 🔸 SIMPLE MUSIC Project
 # 🔹 Developed & Maintained by: Simple Boy (https://github.com/Simple-Boy-1k)
 # 📅 Copyright © 2026 – All Rights Reserved
-#
-# 📖 License:
-# This source code is open for educational and non-commercial use ONLY.
-# You are required to retain this credit in all copies or substantial portions of this file.
-# Commercial use, redistribution, or removal of this notice is strictly prohibited
-# without prior written permission from the author.
-#
-# ❤️ Made with dedication and love by Simple_Boy_1k
 # -----------------------------------------------
 import os
 from random import randint
@@ -85,11 +77,15 @@ async def stream(
                     db[chat_id] = []
                 status = True if video else None
                 try:
-                    file_path, direct = await YouTube.download(
-                        vidid, mystic, video=status, videoid=True
-                    )
+                    file_path = await YouTube.stream_link(vidid, video=status, videoid=True)
+                    direct = True
                 except:
-                    raise AssistantErr(_["play_14"])
+                    try:
+                        file_path, direct = await YouTube.download(
+                            vidid, mystic, video=status, videoid=True
+                        )
+                    except:
+                        raise AssistantErr(_["play_14"])
                 await SIMPLE.join_call(
                     chat_id,
                     original_chat_id,
@@ -148,12 +144,18 @@ async def stream(
         duration_min = result["duration_min"]
         thumbnail = result["thumb"]
         status = True if video else None
+        
         try:
-            file_path, direct = await YouTube.download(
-                vidid, mystic, videoid=True, video=status
-            )
+            file_path = await YouTube.stream_link(vidid, video=status, videoid=True)
+            direct = True
         except:
-            raise AssistantErr(_["play_14"])
+            try:
+                file_path, direct = await YouTube.download(
+                    vidid, mystic, videoid=True, video=status
+                )
+            except:
+                raise AssistantErr(_["play_14"])
+
         if await is_active_chat(chat_id):
             await put_queue(
                 chat_id,
